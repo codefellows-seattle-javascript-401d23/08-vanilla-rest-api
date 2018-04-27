162bd3dc5b8ca2f6c8c0b12f851395fccecfaa41
Sarah Geyer <sjgeyer@comcast.net>
2018-04-27 15:57:33 -0700

    all tests definitely passing

--- src/lib/res-writer.js
+++ src/lib/res-writer.js
@@ -4,4 +4,5 @@ module.exports = function resWrite(res, statusCode, contentType, body) {
   res.writeHead(statusCode, { 'Content-Type': contentType });
   res.write(body);
   res.end();
+  return undefined;
 };
